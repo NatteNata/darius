39e53f56e668bebc5f895fc992ac90beb2ef9463
NatteNata <natte@yandex.ru>
2025-06-10 21:38:31 +0300

    feat: route loader and pending component

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -4,21 +4,27 @@ import {ExperiencesList} from "@/features/experiences/ExperiencesList.tsx";
 import {InfiniteScroll} from "@/features/shared/components/InfiniteScroll.tsx";
 import {trpc} from "@/router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Index,
-})
+  loader: async ({ context: { trpcQueryUtils } }) => {
+      await trpcQueryUtils.experiences.feed.prefetchInfinite({})
+  },
+});
 
 function Index()
 {
-    const experiencesQuery = trpc.experiences.feed.useInfiniteQuery({}, {
-        getNextPageParam: lastPage => lastPage.nextCursor
-    });
+    const [{pages}, experiencesQuery]= trpc.experiences.feed.useSuspenseInfiniteQuery(
+      {},
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      },
+    );
 
     return (
         <InfiniteScroll onLoadMore={experiencesQuery.fetchNextPage}>
             <ExperiencesList
-                experiences={experiencesQuery.data?.pages.flatMap(page => page.experiences) ?? []}
-                isLoading={experiencesQuery.isLoading || experiencesQuery.isFetchingNextPage}
+                experiences={pages.flatMap(page => page.experiences)}
+                isLoading={experiencesQuery.isFetchingNextPage}
             />
         </InfiniteScroll>
     );
